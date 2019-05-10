fb8ce2e139e0c72dc1820755d38f0624c8fb3e96
Senya <cracker000000@gmail.com>
2019-05-10 22:52:29 +0300

    Deleted useless files

--- migrations/user_username_nullable.py
+++
@@ -1,9 +0,0 @@
-from playhouse.migrate import *
-
-DB_NAME = 'ascension.db'
-db = SqliteDatabase(DB_NAME)
-migrator = SqliteMigrator(db)
-
-migrate(
-    migrator.drop_not_null('user', 'username')
-)
